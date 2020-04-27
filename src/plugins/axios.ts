97a625058f793295503a5d71ab41ed484b18826d
condottieros <condottieros@gmail.com>
2020-04-27 13:41:50 +0700

    обработка ошибок вынесена в интерцепторы

--- src/plugins/axios.ts
+++ src/plugins/axios.ts
@@ -1,3 +1,14 @@
-import axios from 'axios'
+import axios, { AxiosError } from 'axios'
 import { BaseAPIURL } from '@/constats'
-export default axios.create({ baseURL: `${BaseAPIURL}/api/v1`, withCredentials: true })
+const instance = axios.create({ baseURL: `${BaseAPIURL}/api/v1`, withCredentials: true })
+
+
+instance.interceptors.response.use(r => r, err => {
+    if (err.message === 'Network Error') alert('Ошибка соединения! Перезагрузите страницу!')
+    const response = (err as AxiosError).response
+    if (response && response.status >= 500) alert('Ошибка сервера! Перезагрузите страницу!')
+    console.log('axios interceptor  error===>>> ', err)
+    return { data: { result: false } }
+})
+
+export default instance
